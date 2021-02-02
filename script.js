c15a14918c38a75ee439c1765835e569b87c7c26
Marques <MBatoon1996@gmail.com>
2021-02-02 01:29:42 -1000

    display list on load

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
+var tasks;
+
 $.ajax({
     type: 'GET',
     url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=286',
     dataType: 'json',
     success: function(response) {
-        console.log(response);
+        //console.log(response);
+        tasks = response;
+
+        $('p.task').html(tasks.tasks[0].content);
     },
     error: function(request, errorMessage) {
         console.log(errorMessage);
     }
 });
+
+
+var consoleLog = function(){
+    console.log(tasks);
+    console.log(tasks.tasks[0].content);
+
+};
