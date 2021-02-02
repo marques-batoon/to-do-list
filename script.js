4fd2b7226b495086665a5611fb0aa67ac52405e7
Marques <MBatoon1996@gmail.com>
2021-02-02 02:33:48 -1000

    trying to loop list

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-var tasks;
+var taskList;
 
 $.ajax({
     type: 'GET',
     url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=286',
     dataType: 'json',
-    success: function(response) {
-        //console.log(response);
-        tasks = response;
-
-        $('p.task').html(tasks.tasks[0].content);
+    success: function(response) {;
+        taskList = response;
+        for(var i = 0; i < response.tasks.length; i++){
+            $('tbody').append($('<p class="task">' + response.tasks[i].content + '</p>'));
+            $('tbody').append($('<tr class="task">' + response.tasks[i].content + '</tr>'));
+            console.log(response.tasks[i].content);
+        }
+        //$('tr.task').html(taskList.tasks[0].content);
     },
     error: function(request, errorMessage) {
         console.log(errorMessage);
@@ -17,7 +20,7 @@ $.ajax({
 
 
 var consoleLog = function(){
-    console.log(tasks);
-    console.log(tasks.tasks[0].content);
+    console.log(taskList);
+    console.log(taskList.tasks[0].content);
 
 };
